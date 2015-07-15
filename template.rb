d26b0d867072595c7c62233a0ac2e48dd47cbf26
kaito <kalbus1892@gmail.com>
2015-07-15 23:40:59 +0900

    initial commit

---
+++ template.rb
@@ -0,0 +1,19 @@
+# Gemfile
+
+group :development do
+  gem "better_errors"
+  gem "binding_of_caller"
+end
+
+gem 'devise'
+gem 'acts-as-taggable-on'
+gem 'carrierwave', github:'carrierwaveuploader/carrierwave'
+gem "mini_magick"
+gem 'impressionist'
+gem 'fog'
+gem 'slim-rails'
+gem "font-awesome-rails"
+gem 'kaminari'
+gem 'jquery-turbolinks'
+gem 'vuejs-rails'
+gem 'dotenv-rails'
