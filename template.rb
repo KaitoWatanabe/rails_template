8788e3324df28db17195b85d74e41bc907f9e2bf
kaito <kalbus1892@gmail.com>
2015-09-14 12:57:53 +0900

    Add quit-assets-gem

--- template.rb
+++ template.rb
@@ -2,17 +2,17 @@
 gem_group :development do
   gem "better_errors"
   gem "binding_of_caller"
+  gem 'quiet_assets'
 end
 
 gem 'devise'
 gem 'acts-as-taggable-on'
-gem "mini_magick"
 gem 'impressionist'
-gem 'fog'
 gem 'slim-rails'
 gem "font-awesome-rails"
 gem 'kaminari'
 gem 'jquery-turbolinks'
+gem 'cancancan'
 
 # application
 application 'config.i18n.default_locale = :ja'
