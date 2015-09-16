1ec6bf1811f9cf762cafa4700d3cda2471353d94
kaito <kalbus1892@gmail.com>
2015-09-16 21:39:20 +0900

    add better_errors config

--- template.rb
+++ template.rb
@@ -21,13 +21,18 @@ application 'config.i18n.default_locale = :ja'
 generate :controller, "home", "index"
 route "root to: 'home#index'"
 
+initializer 'better_errors.rb', <<-CODE
+if defined? BetterErrors
+  BetterErrors.editor = proc { |file, line| "viminiterm://open?url=file://#{file}&line=#{line}" }
+end
+CODE
+
 after_bundle do
   run "gem install html2slim --no-ri --no-rdoc"
-  run "erb2slim "
+  run "erb2slim app/views/layouts/application.html.erb && rm app/views/layouts/application.html.erb"
 
   # devise
   run "bundle exec spring stop"
-  run "erb2slim app/views/layouts/application.html.erb && rm app/views/layouts/application.html.erb"
   generate 'devise:install'
   environment "config.action_mailer.default_url_options = { host: 'localhost:3000' }", env: 'development'
   generate 'devise:views'
